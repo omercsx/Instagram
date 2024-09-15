806f02d20bd041181c220678287831b5a83536f0
omercsx <omercagris@gmail.com>
2024-09-15 10:38:00 -0400

    data separated

--- App.tsx
+++ App.tsx
@@ -7,58 +7,18 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import FeedPost from './src/components/FeedPost';
-import type { IPost } from './src/Models';
-
-const post: IPost = {
-  id: '1',
-  createdAt: '19 December 2021',
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus unde blanditiis. Eos fugiat dolorem ea fugit aut sapiente corrupti autem dolores deleniti architecto, omnis, amet unde dignissimos quam minima?',
-  user: {
-    id: '1',
-    name: 'Omer',
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    username: 'omercs',
-  },
-  nofComments: 11,
-  nofLikes: 33,
-  comments: [
-    {
-      id: '1',
-      comment: 'Hello there',
-      user: {
-        id: '1',
-        name: 'Omer',
-        image:
-          'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-        username: 'omercs',
-      },
-    },
-    {
-      id: '2',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        id: '2',
-        name: 'John',
-        image:
-          'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/2.jpg',
-        username: 'john123',
-      },
-    },
-  ],
-};
+import { post } from './src/assets/post';
 
 function App(): React.JSX.Element {
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.app}>
       <FeedPost post={post} />
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
     marginVertical: 50,
   },
