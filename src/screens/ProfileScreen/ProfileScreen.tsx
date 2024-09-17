799eca5c0d79b42006b3852a342db3ed22155130
omercsx <omercagris@gmail.com>
2024-09-17 10:05:46 -0400

    Images rendered

--- src/screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Image, FlatList } from 'react-native';
-
 import user from '../../assets/data/user.json';
 import ProfileHeader from './ProfileHeader';
+import FeedGridView from '../../components/FeedGridView';
+import type { IPost } from '../../Models';
 
 const ProfileScreen = () => {
   return (
-    <FlatList
-      data={user.posts}
-      renderItem={({ item }) => (
-        <Image
-          source={{ uri: item.image || item.images?.[0] }}
-          style={{ flex: 1, aspectRatio: 1, margin: 1, maxWidth: '33.33%' }}
-        />
-      )} // {item, index}
-      showsVerticalScrollIndicator={false}
-      numColumns={3}
+    <FeedGridView
+      data={user.posts as IPost[]}
       ListHeaderComponent={ProfileHeader}
     />
   );
