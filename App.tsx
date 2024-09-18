98c98c767556dae228b27c610f4cc10dd4a2b712
omercsx <omercagris@gmail.com>
2024-09-18 15:06:16 -0400

    stack navigation installed

--- App.tsx
+++ App.tsx
@@ -5,27 +5,10 @@
  * @format
  */
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-// import CommentsScreen from './src/screens/CommentsScreen';
-// import HomeScreen from './src/screens/HomeScreen';
-import PostUploadScreen from './src/screens/PostUploadScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './src/navigation';
 
 function App(): React.JSX.Element {
-  return (
-    <NavigationContainer>
-      <View style={styles.app}>
-        <PostUploadScreen />
-      </View>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  app: {
-    flex: 1,
-    marginVertical: 50,
-  },
-});
 
 export default App;
