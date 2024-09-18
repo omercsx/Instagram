8cfa5b1f796970a9533fa0e960b4e7de79fc25af
omercsx <omercagris@gmail.com>
2024-09-18 15:24:24 -0400

    logo added to the header

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import logo from '../assets/images/omercslogo.png';
+import { Image } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Stack.Navigator screenOptions={{ headerShown: true }}>
+        <Stack.Screen
+          name="Feed"
+          component={HomeScreen}
+          options={{ headerTitle: HeaderTitle }}
+        />
+        <Stack.Screen
+          name="UserProfile"
+          component={ProfileScreen}
+          options={{ title: 'Profile' }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
+
+const HeaderTitle = () => {
+  return (
+    <Image
+      source={logo}
+      resizeMode="contain"
+      style={{ width: 150, height: 50 }}
+    />
   );
 };
 
